f65119b7c7df6dd478c7aaea249a0eb4ff802b06
buraxta <burakgulluler@gmail.com>
2024-09-21 19:48:38 +0300

    front: Add activity filters and calendar component; enhance home page layout

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -5,6 +5,7 @@ import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import { router } from "./app/router/route";
 import { store, StoreContext } from "./app/stores/store";
+import "react-calendar/dist/Calendar.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
